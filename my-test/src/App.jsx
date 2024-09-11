ec6b1e4c1c4c2afe2beb647dd844e4575be3fea5
stevenrhett <79732886+stevenrhett@users.noreply.github.com>
2024-09-11 14:48:32 -0700

    commit sh files

--- my-test/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="text-center text-2xl bg-yellow-200 font-bold text-blue-500">
-      Welcome to Tailwind CSS with DaisyUI!
-    </div>
-  );
-}
-
-export default App;
